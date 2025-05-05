19b0542d126cda3aa2940307f641f689abb6289d
iqballa24 <iqbalnugraha347@gmail.com>
2025-05-05 10:20:00 +0700

    feat(component): WIRA-5 #time 1m update dialog component

--- src/components/elements/dialog/Dialog.tsx
+++ src/components/elements/dialog/Dialog.tsx
@@ -44,7 +44,7 @@ const DialogContent = React.forwardRef<
       {...props}
     >
       {children}
-      <DialogPrimitive.Close className="absolute right-4 top-4 rounded-sm opacity-70 ring-offset-background transition-opacity hover:opacity-100 focus:outline-none focus:ring-2 focus:ring-offset-2 disabled:pointer-events-none">
+      <DialogPrimitive.Close className="absolute right-4 top-4 rounded-full opacity-70 ring-offset-background transition-opacity hover:opacity-100 focus:outline-none focus:ring-2 focus:ring-offset-2 disabled:pointer-events-none">
         <div className="flex size-10 items-center justify-center rounded-full border border-border">
           <X className="size-6 text-black" />
         </div>
