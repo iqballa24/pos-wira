fe32b23d05dd14b8e8130129b121a42b7997bcca
iqballa24 <iqbalnugraha347@gmail.com>
2025-05-06 22:10:36 +0700

    feat(feature): WIRA-8 #time 1m update empty tag with fragment

--- src/components/layouts/cashier-layout/CashierLayout.tsx
+++ src/components/layouts/cashier-layout/CashierLayout.tsx
@@ -1,3 +1,5 @@
+import { Fragment } from 'react';
+
 import BottomBar from '@/components/layouts/cashier-layout/bottom-bar';
 import TopBar from '@/components/layouts/cashier-layout/top-bar';
 
@@ -7,13 +9,11 @@ const CashierLayout = ({
   children: React.ReactNode;
 }>) => {
   return (
-    <>
+    <Fragment>
       <TopBar />
-
       <main className="pt-[56px] md:pt-[70px]">{children}</main>
-
       <BottomBar />
-    </>
+    </Fragment>
   );
 };
 
