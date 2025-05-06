dc46d31f8cb78074db5b379c936d28575ed588ab
iqballa24 <iqbalnugraha347@gmail.com>
2025-05-06 22:07:08 +0700

    feat(feature): WIRA-8 #time 1m update empty tag with fragment

--- src/features/menu/index.tsx
+++ src/features/menu/index.tsx
@@ -1,17 +1,19 @@
 'use client';
 
+import { Fragment } from 'react';
+
 import { Container } from '@/components/elements';
 
 import { LastOrderSection, ListMenuSection } from '@/features/menu/sections';
 
 export default function FeatureMenu() {
   return (
-    <>
+    <Fragment>
       <LastOrderSection />
       <Container className="flex w-full flex-col justify-between gap-5 divide-x-8 divide-background xl:flex-row">
         <ListMenuSection />
         <h1 className="w-3/12">hello world</h1>
       </Container>
-    </>
+    </Fragment>
   );
 }
